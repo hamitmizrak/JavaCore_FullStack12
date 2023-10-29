cb33e61623f1a9f0c2799ae6bec8317ee7ce981e
HamitMizrak <hamitmizrak@gmail.com>
2023-10-29 16:55:07 +0300

    _04_Scanner_JOptionalPane_Escape_System.java

--- src/main/java/com/hamitmizrak/javase/_04_Scanner_JOptionalPane_Escape_System.java
+++ src/main/java/com/hamitmizrak/javase/_04_Scanner_JOptionalPane_Escape_System.java
@@ -1,8 +1,29 @@
 package com.hamitmizrak.javase;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class _04_Scanner_JOptionalPane_Escape_System {
     public static void main(String[] args) {
 
+        // Scanner
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Adınız ve soyadınız");
+        String value=scanner.nextLine();
+        System.out.println(value);
+        System.err.println(value);
+
+        // JOptionPane
+        String value2= JOptionPane.showInputDialog("Adınız ve soyadınız");
+        System.out.println(value2);
+
+        // System
+        /*
+        System.in();
+        System.out();
+        System.gc();
+        System.currentTimeMillis();
+        */
 
     }
 }
