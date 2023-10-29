e9303f680eb057f13a0a5d081e0105f55c81fe7f
HamitMizrak <hamitmizrak@gmail.com>
2023-10-29 16:25:00 +0300

    _02_Variable.java and Common Rules

--- src/main/java/com/hamitmizrak/javase/_02_Variable.java
+++ src/main/java/com/hamitmizrak/javase/_02_Variable.java
@@ -3,5 +3,39 @@ package com.hamitmizrak.javase;
 public class _02_Variable {
     public static void main(String[] args) {
 
+        // Stack memory- Heap Memory
+        // primitive  type (ilke Type)
+        // ----------------------------
+        // byte
+        // short
+        // int
+        // long
+        byte b1= +127;
+        System.out.println(b1);
+
+        short s1= +128;
+        System.out.println(s1);
+        int i1=5555;
+        long l1=12345611111L;
+
+        // float
+        float f1=14.53f;
+        // double
+        double d1=1221.454;
+
+        // boolean
+        boolean b2=true;
+        // char
+        char c1='4';
+        System.out.println(c1);
+
+        // Common Rules
+        int variableData=14;
+        int variableData44=14;
+        int _variableData44=14;
+        int $variableData44=14;
+        // yazma üğşçöı
+        //int -+~variableData44=14;
+
     }
 }
