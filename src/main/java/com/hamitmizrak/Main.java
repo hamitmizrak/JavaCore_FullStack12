0901dc00603a2cf395fe9f61aa6187b7c5e4fbe0
HamitMizrak <hamitmizrak@gmail.com>
2023-10-29 15:57:13 +0300

    _01_WhatIsJava.java

--- src/main/java/com/hamitmizrak/Main.java
+++ src/main/java/com/hamitmizrak/Main.java
@@ -3,6 +3,6 @@ package com.hamitmizrak;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("üğşçö");
     }
 }
---
+++ src/main/java/com/hamitmizrak/javase/_01_WhatIsJava.java
@@ -0,0 +1,15 @@
+package com.hamitmizrak.javase;
+
+public class _01_WhatIsJava {
+    // Neden Java Öğreniyorum ?
+    // Compiler
+    // Interpreter
+    // JIT
+    // JVM
+    // JRE
+    // JDK
+    // Byte Codes
+    // Java Life Cycle
+    // Ascii character
+    // Unicode Chracter
+}
