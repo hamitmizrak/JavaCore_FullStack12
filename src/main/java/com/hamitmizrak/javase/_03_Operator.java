a03ea50f46edc16c7c4612c1db95a6477f66bac1
HamitMizrak <hamitmizrak@gmail.com>
2023-10-29 16:28:47 +0300

    _03_Operator

---
+++ src/main/java/com/hamitmizrak/javase/_03_Operator.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak.javase;
+
+public class _03_Operator {
+    public static void main(String[] args) {
+
+        // + - * / %
+        // &&=VE ||=VEYA !=Değil
+        int number1 = 25;
+        int number2 = 3;
+        System.out.println("Toplama: " + (number1 + number2));
+        System.out.println((number1 - number2));
+        System.out.println((number1 * number2));
+        System.out.println((number1 / number2));
+        System.out.println((number1 % number2));
+    }
+}
